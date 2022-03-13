e2e91f082ef0db85f116e8e83e287600e67d3ca0
betancourtg <ada_hack140@hackademy.mx>
2022-03-12 19:55:06 -0600

    suma arreglo
    Función para sumar un arreglo

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
  y = int(input("Ingresa el valor de y: "))
  suma = x + y
 print("El total de la suma es: ")
+
+# python1_suma_arreglo
+import random
+arreglo = []
+for i in range(100):
+  arreglo.append(random.randint(1,10))
+print("Arreglo: ", arreglo)
+
+suma = 0
+for valor in arreglo:
+      suma = suma + valor
+print(f"La suma del arreglo es {suma} ")
--- python_suma.py
+++ python_suma.py
@@ -1,4 +1,17 @@
+# python1_suma
 x = int(input("Ingresa x: "))
 y = int(input("Ingresa y: "))
 suma = x + y
 print("El total de la suma es: ", suma)
+
+# python1_suma_arreglo
+import random
+arreglo = []
+for i in range(100):
+  arreglo.append(random.randint(1,10))
+print("Arreglo: ", arreglo)
+
+suma = 0
+for valor in arreglo:
+      suma = suma + valor
+print(f"La suma del arreglo es {suma} ")
